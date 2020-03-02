5045f5e903c4cb7d9b60d3d3cd0af86198eb6861
Mask <vint.max@yandex.ru>
2020-03-02 21:46:22 +0400

    create repository

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var gulp = require("gulp");
+var del = require("del");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var posthtml = require("gulp-posthtml");
+var rename = require("gulp-rename");
+var server = require("browser-sync").create();
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{ttf}",
+      "source/img/**",
+      "source/js/**"
+    ], {
+      base: "source"
+   })
+   .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series("clean", "copy", "html", "css"));
+gulp.task("start", gulp.series("build", "server"));
